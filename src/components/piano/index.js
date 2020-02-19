34d59fab09a7199ff129c06f1f271e19bfc6e613
Brian Paintsil <brnpaintsil@gmail.com>
2020-02-19 23:56:35 +0000

    fix highlightin

--- src/components/piano/index.js
+++ src/components/piano/index.js
@@ -6,28 +6,36 @@ import PianoScale from './scale';
 import PianoKeys from './keys';
 
 // styles
-import './style.css'
+import './style.css';
 
 const Piano = () => {
-    const onStart = ({ note, octave}) => {
+    const onStart = ({ note, octave }) => {
         console.log(`Note ${note}${octave} was played!`);
-    }
+    };
 
-    const onStop = () => {
-
-    }
+    const onStop = () => {};
 
     return (
         <>
-            <div className="piano">
+            <div className='piano'>
                 <PianoBrand />
                 <PianoScale>
                     <PianoKeys onStart={onStart} onStop={onStop} />
                 </PianoScale>
             </div>
-            
+
+            <div className='piano__play__container'>
+                <input
+                    type='text'
+                    className='piano__play__input'
+                    placeholder='Type notes to play'
+                />
+                <button type='button' className='piano__play__btn'>
+                    Play
+                </button>
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Piano;
