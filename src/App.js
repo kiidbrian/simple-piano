35e5bf8f4cc9a29fc37d4d6a72e86806d25bd002
Brian Paintsil <brnpaintsil@gmail.com>
2020-02-19 20:17:16 +0000

    filling in the layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Simple Piano</h1>
+      <h3>Simple Piano</h3>
+
+      <div className="piano">
+        <button className="white-key C1-key"></button>
+        <button className="black-key Db1-key"></button>
+        <button className="white-key Db-key"></button>
+        <button className="black-key Eb1-key"></button>
+        <button className="white-key E1-key"></button>
+        <button className="white-key F1-key"></button>
+        <button className="black-key Gb1-key"></button>
+        <button className="white-key G1-key"></button>
+        <button className="black-key Ab1-key"></button>
+        <button className="white-key A1-key"></button>
+        <button className="black-key Bb1-key"></button>
+        <button className="white-key B1-key"></button>
+      </div>
     </div>
   );
 }
